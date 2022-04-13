54f918b154e67dc1ee582d5bc13da83d81cf1d41
minh-p <75456861+minh-p@users.noreply.github.com>
2022-04-13 12:54:14 -0700

    Delete elpa directory

--- elpa/undo-fu-20220412.2337/undo-fu-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from undo-fu.el  -*- no-byte-compile: t -*-
-(define-package "undo-fu" "20220412.2337" "Undo helper with redo" '((emacs "25.1")) :commit "c92261a3b7fa8dcb62859f08d85f6e670221c743" :authors '(("Campbell Barton" . "ideasman42@gmail.com")) :maintainer '("Campbell Barton" . "ideasman42@gmail.com") :url "https://gitlab.com/ideasman42/emacs-undo-fu")
